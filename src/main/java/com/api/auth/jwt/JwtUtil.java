1239e072dd6ae7506805636d73fd072ca5bb31d1
hallyson <aparecidohallyson@gmail.com>
2025-02-27 09:48:27 -0300

    configurando jwt token

--- src/main/java/com/api/auth/jwt/JwtUtil.java
+++ src/main/java/com/api/auth/jwt/JwtUtil.java
@@ -1,9 +1,42 @@
 package com.api.auth.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+@Component
 public class JwtUtil {
 
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Long expirationMs;
+
+    private SecretKey secretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username, String crm, String expertise){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", username);
+        claims.put("crm", crm);
+        claims.put("expertise", expertise);
+
+        return Jwts.builder().setClaims(claims).
+                setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+ expirationMs)).
+                signWith(secretKey(), SignatureAlgorithm.HS256).compact();
+    }
 
 }
