a4e96ab649d3eed3f48a4d9cab709ab12c7630b4
hallyson <aparecidohallyson@gmail.com>
2025-02-27 09:41:23 -0300

    config permission routes

---
+++ src/main/java/com/api/auth/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.api.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/doctors" ).permitAll()
+                        .anyRequest().authenticated()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+}
